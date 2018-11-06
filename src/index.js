1198a9f0229349ba956f7cc33f48562cd533aa6b
Matthijs Groen <matthijs@kabisa.nl>
2018-11-06 21:15:59 +0100

    Start validation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import "./index.css";
 const ace = require("brace");
 require("brace/mode/plain_text");
 require("brace/theme/monokai");
-const ebnf = require("ebnf2railroad");
-console.log(ebnf);
+const { parseEbnf, validateEbnf } = require("ebnf2railroad");
 
 const editor = ace.edit("editor");
 editor.getSession().setMode("ace/mode/plain_text");
@@ -13,8 +12,29 @@ editor.getSession().on("change", () => {
   const newValue = editor.getValue();
   try {
     const ast = parseEbnf(newValue);
+    validateAst(ast);
+    updateAst(ast);
   } catch (e) {
-    console.log(e.message);
+    const { expected, line, token } = e.hash;
+    editor.getSession().clearAnnotations();
+    editor.getSession().setAnnotations([
+      {
+        text: `Expected ${expected}, got ${token}`,
+        type: "error",
+        column: 0,
+        row: line
+      }
+    ]);
   }
-  console.log(newValue);
 });
+
+const validateAst = ast => {
+  try {
+    const result = validateEbnf(ast);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const updateAst = ast => {};
