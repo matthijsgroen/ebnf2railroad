0d1265c96af08ecbf94277cff6abb83b9112320d
Matthijs Groen <matthijs@kabisa.nl>
2018-11-07 14:01:13 +0100

    Start syntax highlighting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,111 @@
 import "./try-yourself.css";
 
 const ace = require("brace");
+
+ace.define(
+  "ace/mode/ebnf_highlight_rules",
+  ["require", "exports", "ace/lib/oop", "ace/mode/text_highlight_rules"],
+  function(getDep, result) {
+    function SyntaxHighlighter() {
+      this.$rules = {
+        start: [
+          {
+            token: "comment",
+            regex: "\\(\\*",
+            next: "comment"
+          },
+          {
+            token: "paren.lparen",
+            regex: "[({[]"
+          },
+          {
+            statename: "qstring",
+            token: "string.start",
+            regex: "'",
+            next: [
+              {
+                token: "string.end",
+                regex: "'",
+                next: "start"
+              },
+              {
+                defaultToken: "string"
+              }
+            ]
+          },
+          {
+            statename: "qqstring",
+            token: "string.start",
+            regex: '"',
+            next: [
+              {
+                token: "string.end",
+                regex: '"',
+                next: "start"
+              },
+              {
+                defaultToken: "string"
+              }
+            ]
+          }
+        ],
+        comment: [
+          {
+            token: "comment",
+            regex: "\\*\\)",
+            next: "start"
+          },
+          {
+            defaultToken: "comment"
+          }
+        ]
+      };
+      this.normalizeRules();
+    }
+    const oop = getDep("../lib/oop");
+    const parent = getDep("./text_highlight_rules").TextHighlightRules;
+    oop.inherits(SyntaxHighlighter, parent);
+    result.EBNFHighlightRules = SyntaxHighlighter;
+  }
+);
+
+ace.define(
+  "ace/mode/ebnf",
+  [
+    "require",
+    "exports",
+    "module",
+    "ace/mode/ebnf_highlight_rules",
+    "ace/mode/text",
+    "ace/lib/oop"
+  ],
+  function(getDep, result) {
+    const highlighter = getDep("./ebnf_highlight_rules").EBNFHighlightRules;
+
+    const Mode = function() {
+      // constructor
+      this.HighlightRules = highlighter;
+    };
+
+    const oop = getDep("../lib/oop");
+    const parent = getDep("./text").Mode;
+
+    oop.inherits(Mode, parent);
+    (function() {
+      // overrides after inheritance
+      this.$id = "ace/mode/ebnf";
+      this.blockComment = {
+        start: "(*",
+        end: "*)"
+      };
+    }.call(Mode.prototype));
+
+    result.Mode = Mode;
+  }
+);
+
 const EditSession = ace.EditSession;
-require("brace/mode/plain_text");
-require("brace/theme/monokai");
+require("brace/theme/twilight");
 const {
   parseEbnf,
   validateEbnf,
@@ -73,8 +175,8 @@ const headSection = document.getElementsByTagName("head")[0];
 headSection.appendChild(styleElem);
 
 editor.setSession(session);
-editor.getSession().setMode("ace/mode/plain_text");
-editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/ebnf");
+editor.setTheme("ace/theme/twilight");
 editor.session.setTabSize(2);
 editor.session.setUseSoftTabs(true);
 editor.setHighlightActiveLine(true);
