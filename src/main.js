872abb120bf8a9e728a8ea4cbf8e67a367ad8d21
Matthijs Groen <matthijs@kabisa.nl>
2018-11-07 22:35:25 +0100

    Add reference functions to interface

--- src/main.js
+++ src/main.js
@@ -5,11 +5,19 @@ const {
   validateEbnf
 } = require("./report-builder");
 const { version } = require("../package.json");
+const {
+  getReferences,
+  searchReferencesFromIdentifier,
+  searchReferencesToIdentifier
+} = require("./references");
 
 module.exports = {
   version,
   parseEbnf: parse,
   createDocumentation,
   documentStyle,
-  validateEbnf
+  validateEbnf,
+  getReferences,
+  searchReferencesFromIdentifier,
+  searchReferencesToIdentifier
 };
